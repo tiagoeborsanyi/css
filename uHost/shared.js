617c2a7dbc2f936e111d25f13bf080e76404626c
Tiago Emerick <tiagoeborsanyi@gmail.com>
2020-04-24 16:34:03 -0300

    add js events for mobile menu - uhost

--- uHost/shared.js
+++ uHost/shared.js
@@ -2,6 +2,8 @@ const backdrop = document.querySelector('.backdrop');
 const modal = document.querySelector('.modal');
 const modalNoButton = document.querySelector('.modal__action--negative');
 const selectPlanButton = document.querySelectorAll('.plan button');
+const toggleButton = document.querySelector('.toggle-button');
+const mobileNav = document.querySelector('.mobile-nav');
 
 for (let i = 0; i < selectPlanButton.length; i++) {
     selectPlanButton[i].addEventListener('click', () => {
@@ -10,10 +12,19 @@ for (let i = 0; i < selectPlanButton.length; i++) {
     });
 }
 
-backdrop.addEventListener('click', closeModal);
+backdrop.addEventListener('click', () => {
+    mobileNav.style.display = 'none';
+    closeModal();
+});
+
 modalNoButton.addEventListener('click', closeModal);
 
 function closeModal() {
     modal.style.display = 'none';
     backdrop.style.display = 'none';
 }
+
+toggleButton.addEventListener('click', () => {
+    mobileNav.style.display = 'block';
+    backdrop.style.display = 'block';
+})
