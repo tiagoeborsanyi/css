387b3fef1a6a4c5b32c3a851a6de33ca5f5d4de2
Tiago Emerick <tiagoeborsanyi@gmail.com>
2020-04-24 17:05:00 -0300

    finish basic layout mobile menu - uhost

--- uHost/shared.js
+++ uHost/shared.js
@@ -7,24 +7,36 @@ const mobileNav = document.querySelector('.mobile-nav');
 
 for (let i = 0; i < selectPlanButton.length; i++) {
     selectPlanButton[i].addEventListener('click', () => {
-        modal.style.display = 'block';
-        backdrop.style.display = 'block';
+        // modal.style.display = 'block';
+        // backdrop.style.display = 'block';
+        // modal.className = 'open'; this will actualy overwrite the complete line
+        modal.classList.add('open');
+        backdrop.classList.add('open');
     });
 }
 
 backdrop.addEventListener('click', () => {
-    mobileNav.style.display = 'none';
+    // mobileNav.style.display = 'none';
+    mobileNav.classList.remove('open');
     closeModal();
 });
 
-modalNoButton.addEventListener('click', closeModal);
+if (modalNoButton) {
+    modalNoButton.addEventListener('click', closeModal);
+}
 
 function closeModal() {
-    modal.style.display = 'none';
-    backdrop.style.display = 'none';
+    // modal.style.display = 'none';
+    // backdrop.style.display = 'none';
+    if (modal) {
+        modal.classList.remove('open');
+    }
+    backdrop.classList.remove('open');
 }
 
 toggleButton.addEventListener('click', () => {
-    mobileNav.style.display = 'block';
-    backdrop.style.display = 'block';
+    // mobileNav.style.display = 'block';
+    // backdrop.style.display = 'block';
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
 })
